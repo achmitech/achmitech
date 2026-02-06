ccb9b22ed7e93247e775a5d5b5de612ad1ed538a
Ayoub Jbili <ayoub.jbili@achmitech.com>
2026-02-06 10:35:54 +0100

    [REFACTOR] change model to hr.employee since hr.employee.base is removed in v19

--- achmitech_portal_timesheets/models/hr_employee.py
+++ achmitech_portal_timesheets/models/hr_employee.py
@@ -1,6 +1,6 @@
 from odoo import models, fields
 
 class HrEmployeeBase(models.AbstractModel):
-    _inherit = "hr.employee.base"
+    _inherit = "hr.employee"
 
     cra_order_number = fields.Char(string="N° Commande (CRA)")
